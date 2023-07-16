81c1c2588b9f61ba82de6499f0ca961262596139
yeganehNobakht <m.yeganeh.n1994@gmail.com>
2023-07-16 10:31:12 +0330

    initial project

---
+++ src/test/java/com/bmc/spring6authserver/Spring6AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmc.spring6authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6AuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
